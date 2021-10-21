614ca4be66701628388206d7b53b8f773731071b
njumen72 <njumen@int.pl>
2021-10-21 20:17:33 +0200

    delate

--- main.cpp
+++ main.cpp
@@ -4,31 +4,27 @@ using namespace std;
 
 int main()
 {
-	int x, i = 2;
-
-	cout << "Wpisz liczbe: ";
-	cin >> x;
-	cout << endl << x;
-
-	if (x == i)
-		cout << " jest liczba pierwsza" << endl;
-	else {
-		do
-		{
-			if ((x % i == 0) || (x <= 1))
-			{
-				cout << " nie";
-				break;
-			}
-			else
-				i++;
-		} while (x > i);
-
-		cout << " jest liczba pierwsza" << endl;
-	}
-
-	cout << endl;
-	system("pause");
-	return 0;
+    int x, i = 2;
+    cout << "Wpisz liczbe: ";
+    cin >> x;
+    cout << endl << x;
+    if (x == i)
+        cout << " jest liczba pierwsza" << endl;
+    else {
+        do
+        {
+            if ((x % i == 0) || (x <= 1))
+            {
+                cout << " nie";
+                break;
+            }
+            else
+                i++;
+        } while (x > i);
+        cout << " jest liczba pierwsza" << endl;
+    }
+    cout << endl;
+    system ("pause");
+    return 0;
 }
 //by njumen72
